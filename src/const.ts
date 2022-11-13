fbc8916d5920b619dd2f06fe16f14e562fe0e5a2
Youvandra Febrial <youvan@MacBook-Pro-Youvandra.local>
2022-11-13 19:12:09 +0700

    ArtBeatApp

---
+++ src/const.ts
@@ -0,0 +1,20 @@
+export const YOUTUBE_VIDEO =
+  "https://www.youtube.com/embed/dQw4w9WgXcQ?controls=0?autoplay=1&mute=0";
+
+export const CONTRACT_ADDRESS = "0x2480aEA0FfABcF3bb8b92E6819B4201717f68BF4";
+
+export const PINATA_KEY = "adcd2f6925a3ad5af235";
+
+export const PINATA_SECRET =
+  "01360437e92b8943c5f7483344d03be0194004554d79615aec26cdec5cb007b7";
+
+export const CHAIN_ID = "0x405";
+
+export const CHAIN_PARAMS = {
+  chainName: "BitTorrent Chain Donau",
+  chainId: CHAIN_ID,
+  nativeCurrency: { name: "BTT", decimals: 18, symbol: "BTT" },
+  rpcUrls: ["https://pre-rpc.bt.io/"],
+  blockExplorerUrls: ["https://testscan.bt.io"],
+  iconUrls: ["https://static.bt.io/production/logo/1002000.png"],
+};
