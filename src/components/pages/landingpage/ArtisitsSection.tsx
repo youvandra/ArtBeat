dd14b8419e49dafac4960e19a1b82321731200af
Youvandra Febrial <kucingplaygame@gmail.com>
2022-11-14 00:11:24 +0700

    carousel fix

--- src/components/pages/landingpage/ArtisitsSection.tsx
+++ src/components/pages/landingpage/ArtisitsSection.tsx
@@ -32,18 +32,17 @@ export default function ArtisitsSection() {
         </Center>
       ) : (
         <CustomCarousel>
-          <Carousel.Slide>
-            {data &&
-              data.map(({ user, id }, i) => (
+          {data &&
+            data.map(({ user, id }, i) => (
+              <Carousel.Slide key={i}>
                 <ArtistCard
-                  key={i}
                   image={user.image}
                   name={user.name}
                   id={id}
                   bgImage="https://images.unsplash.com/photo-1569172122301-bc5008bc09c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
                 />
-              ))}
-          </Carousel.Slide>
+              </Carousel.Slide>
+            ))}
         </CustomCarousel>
       )}
     </Box>
