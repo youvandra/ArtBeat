31b1d38544a03da643ee67caecd2cbab4b267c21
fwwz-id <fwwz.id@proton.me>
2023-09-11 23:31:34 +0700

    chore: redesign current-artwork card

--- src/components/pages/landingpage/lib/CurrentArtworkCard/CurrentArtworkCard.tsx
+++ src/components/pages/landingpage/lib/CurrentArtworkCard/CurrentArtworkCard.tsx
@@ -1,37 +1,64 @@
-import { Avatar, Card, Group, Image, Text, ThemeIcon } from "@mantine/core";
+import {
+  Button,
+  Card,
+  Group,
+  Image,
+  Space,
+  Stack,
+  Text,
+  useMantineTheme,
+} from "@mantine/core";
 
-import { HiOutlineCheckBadge } from "react-icons/hi2";
+// import { HiOutlineCheckBadge } from "react-icons/hi2";
 import { NFT } from "../../../../EventCard";
 
 const CurrentArtworkCard = ({ metadata, ...nft }: NFT) => {
+  const theme = useMantineTheme();
+
   return (
     <Card
       sx={{
         width: 370,
-        height: 300,
+        // height: 300,
       }}
     >
       <Card.Section>
         <Image
           src={metadata.image}
           height={235}
-          sx={{ objectFit: "contain" }}
+          sx={{
+            // @ts-ignore
+            img: {
+              objectFit: "contain !important",
+            },
+          }}
         />
       </Card.Section>
 
-      <Group mt="-1rem">
-        <Avatar src={null} size={50} sx={{ borderRadius: 99999 }} />
+      <Stack spacing={4} mt={4}>
         <Text
+          size={20}
           sx={{
-            maxWidth: 200,
+            maxWidth: 285,
             textOverflow: "ellipsis",
             whiteSpace: "nowrap",
             overflow: "hidden",
+            color: theme.colors["ocean-blue"][3],
+            fontWeight: 700,
+            fontFamily: theme.headings.fontFamily,
           }}
         >
-          {metadata.artist}
+          {metadata.title}
         </Text>
-        <HiOutlineCheckBadge size={30} color="#0DBBFC" />
+
+        <Text>Art by: {metadata.artist}</Text>
+      </Stack>
+
+      <Space h="xl"/>
+
+      <Group position="apart">
+        <Text weight={700}>{nft.price} BTT</Text>
+        <Button>See Details</Button>
       </Group>
     </Card>
   );
